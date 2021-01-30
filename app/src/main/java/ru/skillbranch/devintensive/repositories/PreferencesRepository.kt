af84fed495896e05060900a58f7b1b1bdeae9a16
Konstantin Kurtukov <konstantinkg@gmail.com>
2021-01-30 11:15:55 +0600

    hometask_4 complete

--- app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
@@ -47,7 +47,7 @@ object PreferencesRepository {
         when (value) {
             is String -> putString(key, value)
             is Int -> putInt(key, value)
-            else -> error("Only primitives types")
+            else -> error("Only primitives types can used")
         }
         apply()
     }
